ea6384d5961df390a706be4e974f25399d5bcd21
Aldo <aldorgarcia29@gmail.com>
2022-11-03 12:39:30 -0400

    cleanup

--- server/dist/middleware/not-found.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const notFoundMiddlware = (req, res, next) => res.status(404).send("Route not found - from not-found MIDDLEWARE");
-exports.default = notFoundMiddlware;
